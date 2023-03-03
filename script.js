81f3c27574a636236e54b23efaabec62877c2137
MrMirka <aleshin.dev@gmail.com>
2023-03-03 15:58:32 +0400

    daily upload

--- script.js
+++ script.js
@@ -53,6 +53,7 @@ renderer.shadowMap.enabled = true
 renderer.shadowMap.type = THREE.PCFSoftShadowMap
 renderer.logarithmicDepthBuffer = true
 renderer.autoClear = true
+renderer.toneMappingExposure = 1.2
 renderer.render(scene, camera)
 
 
@@ -68,8 +69,9 @@ gltfLoader.load('./models/disk.gltf', gltf => {
     let model_lvl1 = gltf.scene.children[0]
     let model_lvl2 = gltf.scene.children[1]
     let model_lvl3 = gltf.scene.children[2]
-
-
+    let model_lvl4 = gltf.scene.children[3]
+    let model_lvl5 = gltf.scene.children[4]
+    let model_lvl6 = gltf.scene.children[5]
 
 
     gltf.scene.scale.set(1,1,1)
@@ -81,20 +83,27 @@ gltfLoader.load('./models/disk.gltf', gltf => {
     innerRoot.add(model_lvl3)
 
     //const folder = gui.addFolder("inner")
-    //folder.add(root.rotation,'y').min(0).max(3).step(0.01).name('rotation y')
-    
-
-    //innerRoot.rotation.y = 0.18
-
-    //innerRoot.position.x = 0.005
+    //folder.add(model_lvl6.rotation,'z').min(-3).max(3).step(0.01).name('rotation z')
    
 
+    model_lvl4.position.set(0.258,0,0.12)
+    model_lvl4.rotation.set(1.6,0,-0.19)
+    model_lvl4.scale.set(0.86,1,0.89)
+
+    model_lvl2.scale.set(0.85,1,0.85)
+
+    model_lvl6.rotation.z = -0.26
+
+  
     root.rotation.y = 0.34
     root.position.x = -0.021
 
     scene.add(root)
     scene.add(model_lvl1)
     scene.add(innerRoot)
+    scene.add(model_lvl4)
+    scene.add(model_lvl5)
+    scene.add(model_lvl6)
    
     
 })
@@ -105,9 +114,7 @@ rgbloader.load('./models/texture/env/env.pic', texture => {
     texture.wrapS = THREE.RepeatWrapping
     texture.wrapP = THREE.RepeatWrapping
     texture.repeat.set( 1, 1 )
-    scene.environment = texture
-  
-    
+    //scene.environment = texture
  })
  
 
@@ -115,15 +122,19 @@ rgbloader.load('./models/texture/env/env.pic', texture => {
  pointLight.position.set( 1, 1, 1 )
  pointLight.castShadow = true
  pointLight.shadow.normalBias = 0.05
- scene.add( pointLight )
+ //scene.add( pointLight )
  
  const sphereSize = 0.2
  const pointLightHelper = new THREE.PointLightHelper( pointLight, sphereSize );
- scene.add( pointLightHelper )
+ //scene.add( pointLightHelper )
 
 // document.addEventListener('mousedown',() => {
 //     changeMaterial(urls)
 // });
+
+const light = new THREE.AmbientLight( 0x404040 );
+light.intensity = 10
+scene.add( light );
 
 
 animate()
@@ -136,7 +147,6 @@ function animate(){
 
    if(screen_mesh != undefined) {
     screen_mesh.rotation.y += 0.02
-   // screen_mesh.rotation.set(screen_mesh.rotation.y, degree, screen_mesh.rotation.z)
    }
     
     requestAnimationFrame(animate)
